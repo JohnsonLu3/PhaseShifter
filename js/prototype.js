bd17fad63fef4c9eb5ac314968629a23d0fecb81
JohnsonLu3 <JohnsonLu94@gmail.com>
2017-04-03 14:08:42 -0400

    added level and level tilesheet.

--- js/prototype.js
+++ js/prototype.js
@@ -1,13 +1,39 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var w = 2560;
+var h = 640;
+var player; 
 
 function preload() {
-
+    game.load.tilemap('mapdata', 'assets/levels/level0.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tiles', 'assets/levels/tilesheet.png');
+    game.load.spritesheet('player', "assets/phaser.png", 32,32);
 }
 
-function create() {
+var map;
+var layer;
 
+function create() {
+    game.stage.backgroundColor = '#787878';
+
+    // Load level from mapdata
+    map = game.add.tilemap('mapdata');
+    map.addTilesetImage('tilesheet', 'tiles');
+
+    // start physics and set up cursor
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    cursors = game.input.keyboard.createCursorKeys();
+
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    //game.world.setBounds(0, 0, w, h);
+
+    cursors = game.input.keyboard.createCursorKeys();
+
+    layer = map.createLayer('mapdata');
+    // create player
+    player = new Player();
+    //game.camera.follow(player);
 }
 
 function update() {
-    
+    //game.physics.arcade.collide(player, layer);
 }
