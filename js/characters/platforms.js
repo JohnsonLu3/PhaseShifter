7e1827a8379eb7f0534ac3fd33e8c14b98e94c6c
jonapi <jonapi10@gmail.com>
2017-04-27 00:20:51 -0400

    added a flickering effect to platforms that are about to change

--- js/characters/platforms.js
+++ js/characters/platforms.js
@@ -10,6 +10,8 @@ var Platform = function (game, x, y, interval, state) {
     game.physics.arcade.enable(this);
     this.body.immovable = true;
     this.body.setSize(60, 8, 2, 12);
+    this.flicker = false;
+    this.animations.add("flicker", [0,1], 30, true);
     
 }
 
@@ -17,15 +19,31 @@ Platform.prototype = Object.create(CustomSprite.prototype);
 Platform.prototype.constructor = Platform;
 Platform.prototype.changePhase = function () {
     if (this.interval != 0) {
+        //One second in warning before platform will change.
+        if ((globalTimer % this.interval) - this.interval > -60 && (globalTimer % this.interval) - this.interval < 0)
+        {
+            this.flicker = true;
+        }
+        else
+        {
+            this.flicker = false;
+        }
         if (globalTimer % this.interval == 0 && globalTimer != this.lastShift) {
             this.shiftState = !this.shiftState;
             this.lastShift = globalTimer;
         }
     }
-    if (this.shiftState) {
-        this.loadTexture('platform', 1);
+    if (!this.flicker)
+    {
+        if (this.shiftState) {
+            this.loadTexture('platform', 1);
+        }
+        else {
+            this.loadTexture('platform', 0);
+        }
     }
-    else {
-        this.loadTexture('platform', 0);
+    else
+    {
+        this.animations.play("flicker");
     }
 }
